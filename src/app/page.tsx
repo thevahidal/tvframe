f2ec2a7f024fd03d93daadb0cbe0ef6d74a23508
Vahid Al <thevahidal@gmail.com>
2023-11-02 00:18:08 +0330

    Update iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,38 @@
 "use client"
+import React from 'react';
 
-import { useEffect, useRef } from 'react'
-import styles from './page.module.css'
-
-const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
-
-export default function Home() {
-  const iframeRef = useRef();
-
-  useEffect(() => {
-    const userAgentProp = {
-      get: function() {
-        return USER_AGENT;
-      }
-    };
-
-    iframeRef.current.navigator = Object.create(navigator, {
-      userAgent: userAgentProp
-    });
-  })
+const Home: React.FC = () => {
+  const iframeSrc = 'https://filmnet.ir/';
+  const useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
 
   return (
-      <iframe ref={iframeRef} className={styles.iframe} src="https://filmnet.ir/" />
-  )
-}
+    <iframe
+      src={iframeSrc}
+      style={{ width: '100%', height: '100vh' }}
+      sandbox="allow-same-origin allow-scripts allow-popups"
+      title="FilmNet"
+      onLoad={(e) => {
+        const iframe = e.target as HTMLIFrameElement;
+        const doc = iframe.contentWindow?.document;
+        if (doc) {
+          doc.open();
+          doc.write('<!DOCTYPE html><html><head><title>FilmNet</title></head><body></body></html>');
+          doc.close();
+          doc.documentElement.style.overflow = 'hidden';
+          doc.documentElement.style.width = '100%';
+          doc.documentElement.style.height = '100%';
+          doc.body.style.overflow = 'hidden';
+          doc.body.style.width = '100%';
+          doc.body.style.height = '100%';
+          const script = doc.createElement('script');
+          script.type = 'text/javascript';
+          script.innerHTML = `Object.defineProperty(navigator, 'userAgent', { value: '${useragent}', writable: false });`;
+          doc.head.appendChild(script);
+          doc.location.replace(iframeSrc);
+        }
+      }}
+    />
+  );
+};
+
+export default Home;
